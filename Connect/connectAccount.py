9ebe422d075c5de7563ce672afcdfc4719260bfc
El Habib <Abdelaziz.59140@gmail.com>
2020-01-17 15:31:06 +0100

    add method for verify pseudo and password

--- Connect/connectAccount.py
+++ Connect/connectAccount.py
@@ -12,6 +12,11 @@ class Account():
         self.password = (input("Votre mot de passe : "))
         self.choice.initialize_connection()
         self.choice.cursor.execute("SELECT pseudo, password FROM Users WHERE pseudo = %s AND password = %s;",(self.pseudo,self.password))
-        raws = self.choice.cursor.fectall()
+        raws = self.choice.cursor.fetchall()
 
-        
+        for raw in raws:
+            if raw[0] == self.pseudo and raw[1] == self.password:
+                login = True
+                print(login)
+
+        self.choice.close_connection()
